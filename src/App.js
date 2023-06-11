6bb7f52512761d59e7e31841444ee03f58941516
OrhanT <orhantornaci@gmail.com>
2023-06-11 16:39:08 +0300

    Add Redux integration to the React app

--- src/App.js
+++ src/App.js
@@ -1,35 +1,42 @@
 import {BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
 import logo from './logo.svg';
 import './assets/scss/themes.scss';
 import './App.css';
 import AppContextProvider from '@hap/utility/AppContextProvider';
 import AppLocaleProvider from '@hap/utility/AppLocaleProvider';
 
+import configureStore, {history} from 'redux/store';
+
+const store = configureStore();
+
 function App() {
   return (
     <AppContextProvider>
       <AppLocaleProvider>
-        <BrowserRouter>
-          <div className='App'>
-            <header className='App-header'>
-              <div className='row'>
-                <div className='col'>ddd</div>
-              </div>
-              <img src={logo} className='App-logo' alt='logo' />
-              <p>
-                Edit <code>src/App.js</code> and save to reload.
-              </p>
-              <a
-                className='App-link'
-                href='https://reactjs.org'
-                target='_blank'
-                rel='noopener noreferrer'
-              >
-                Learn React
-              </a>
-            </header>
-          </div>
-        </BrowserRouter>
+        <Provider store={store}>
+          <BrowserRouter history={history}>
+            <div className='App'>
+              <header className='App-header'>
+                <div className='row'>
+                  <div className='col'>ddd</div>
+                </div>
+                <img src={logo} className='App-logo' alt='logo' />
+                <p>
+                  Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                  className='App-link'
+                  href='https://reactjs.org'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  Learn React
+                </a>
+              </header>
+            </div>
+          </BrowserRouter>
+        </Provider>
       </AppLocaleProvider>
     </AppContextProvider>
   );
