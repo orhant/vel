088b21eb85dbae01ee44a42f6811c1a1ce1711e5
OrhanT <orhantornaci@gmail.com>
2023-06-11 04:06:11 +0300

    Add Bootstrap 5 and new assets

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
+import './assets/scss/themes.scss';
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
+        <div className='row'>
+          <div className='col'>ddd</div>
+        </div>
         <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
