fbe222a6c0b1ed364373e241aea5beafdfc2d2c8
OrhanT <orhantornaci@gmail.com>
2023-06-11 22:21:29 +0300

    Add JWTAuthAuthProvider integration to the project

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import AppContextProvider from '@hap/utility/AppContextProvider';
 import AppLocaleProvider from '@hap/utility/AppLocaleProvider';
 
 import configureStore, {history} from 'redux/store';
+import JWTAuthAuthProvider from 'services/auth/jwt-auth/JWTAuthProvider';
 
 const store = configureStore();
 
@@ -16,25 +17,27 @@ function App() {
       <AppLocaleProvider>
         <Provider store={store}>
           <BrowserRouter history={history}>
-            <div className='App'>
-              <header className='App-header'>
-                <div className='row'>
-                  <div className='col'>ddd</div>
-                </div>
-                <img src={logo} className='App-logo' alt='logo' />
-                <p>
-                  Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                  className='App-link'
-                  href='https://reactjs.org'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  Learn React
-                </a>
-              </header>
-            </div>
+            <JWTAuthAuthProvider>
+              <div className='App'>
+                <header className='App-header'>
+                  <div className='row'>
+                    <div className='col'>ddd</div>
+                  </div>
+                  <img src={logo} className='App-logo' alt='logo' />
+                  <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                  </p>
+                  <a
+                    className='App-link'
+                    href='https://reactjs.org'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                  >
+                    Learn React
+                  </a>
+                </header>
+              </div>
+            </JWTAuthAuthProvider>
           </BrowserRouter>
         </Provider>
       </AppLocaleProvider>
